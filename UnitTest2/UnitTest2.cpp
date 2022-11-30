6864c31744257f0825c3c49c8bc9a81519a7020e
Gr1z1 <119458607+Gr1z1@users.noreply.github.com>
2022-11-30 02:54:19 +0200

    Update UnitTest2.cpp

--- UnitTest2/UnitTest2.cpp
+++ UnitTest2/UnitTest2.cpp
@@ -1,16 +1,125 @@
-﻿#include "pch.h"
+#include "pch.h"
 #include "CppUnitTest.h"
+#include "C:\Users\Vitaly\source\repos\ConsoleApplication13\ConsoleApplication13\ConsoleApplication13.cpp"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace UnitTest2
+namespace LABA3
 {
-	TEST_CLASS(UnitTest2)
+	TEST_CLASS(LABA3)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
+		// Passed
+		TEST_METHOD(calculate_n3_x8_get12)
 		{
+			int x = 8;
+			int n = 3;
+			int expected = 12;
+			int received = calculate(n, x);
+			Assert::AreEqual(expected, received);
 		}
+		
+	public:
+		// Passed
+		TEST_METHOD(calculate_n3_x_minus8_get12)
+		{
+			int x = -3;
+			int n = 2;
+			int expected = -5;
+			int received = calculate(n, x);
+			Assert::AreEqual(expected, received);
+		}
+		
+	public:
+		// Unpassed
+		TEST_METHOD(calculate_n2_x_minus3_getERROR)
+		{
+			int x = -3;
+			int n = 2;
+			int expected = -342234;
+			int received = calculate(n, x);
+			Assert::AreEqual(expected, received);
+		}
+		
+	public:
+		// Об'єкти не рівні
+		// Passed
+		TEST_METHOD(calculate_n3_x8_get1234_butAssert_notEqual)
+		{
+			int x = 8;
+			int n = 3;
+			int expected = 1234;
+			int received = calculate(n, x);
+			Assert::AreNotEqual(expected, received);
+		}
+		
+	public:
+		// Unpassed
+		TEST_METHOD(calculate_n3_x_minus4_getERROR)
+		{
+			int x = -4;
+			int n = 2;
+			int expected = -5;
+			int received = calculate(n, x);
+			Assert::AreEqual(expected, received);
+		}
+		
+	public:
+		// Перевірка об'єктів на посилання на інші об'єкти
+		// Passed
+		TEST_METHOD(calculate_n3_x_minus4_testAreNotSame)
+		{
+			int x = -4;
+			int n = 2;
+			int expected = -5;
+			int received = calculate(n, x);
+			Assert::AreNotSame(expected, received);
+		}
+	public:
+		// Passed
+		TEST_METHOD(checkValidParams_get3_exceptionNoThrown)
+		{
+			int n = 3;
+			try
+			{
+				checkValidParams(n);
+				Assert::IsTrue(true);
+			}
+			catch (...)
+			{
+				Assert::Fail();
+			}
+		}
+	public:
+		// Passed
+		TEST_METHOD(checkValidParams_getMinus2_exception_getThrown)
+		{
+			int n = -2;
+			try
+			{
+				checkValidParams(n);
+				Assert::Fail();
+			}
+			catch (...)
+			{
+				Assert::IsTrue(true);
+			}
+		}
+	public:
+		// Passed
+		TEST_METHOD(checkValidParams_getMinus2_exception_getTrown)
+		{
+			int n = 10;
+			try
+			{
+				checkValidParams(n);
+				Assert::IsTrue(true);
+			}
+			catch (...)
+			{
+				Assert::Fail();
+			}
+		}
+	
 	};
 }
